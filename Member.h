1af1bab7df7d6799d35b76e88b4d061ebe03cf7b
avital477 <33714153+avital477@users.noreply.github.com>
2018-04-11 20:46:23 +0300

    with comments

--- Member.h
+++ Member.h
@@ -11,10 +11,6 @@ public:
     Member();
     void follow (Member& member);
     void unfollow (Member& member);
-    // void setFollowers(int f1);
-    // void setFollowing(int f2);
-    // int getFollowers();
-    // int getFollowing();
     int numFollowers() const{ return Followers.size(); }
     int numFollowing() const{ return Following.size(); }
     static int count (){ return counting; }
